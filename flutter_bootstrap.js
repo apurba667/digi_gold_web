89327dd15bcababd1fa2fa3bc74d4c02ca94a52e
apurba <apurbaanikroy@gmail.com>
2025-01-30 12:15:54 +0600

    digital gold section

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1176986023"
+    serviceWorkerVersion: "748388754"
   }
 });
