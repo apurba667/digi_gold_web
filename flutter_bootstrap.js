ed6b6d2dc55d6977a7e005eabd961c19e03b6287
apurba <apurbaanikroy@gmail.com>
2025-02-20 14:55:17 +0600

    product details

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1381835088"
+    serviceWorkerVersion: "25790151"
   }
 });
